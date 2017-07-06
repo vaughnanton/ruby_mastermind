18f067f3bfae5c8d48056cc864f01160360bb2d2
Vaughn Antonyan <vaughnanton@gmail.com>
2017-07-05 23:25:55 -0700

    Skeleton complete

--- mastermind.rb
+++ mastermind.rb
@@ -1,1 +1,31 @@
-#initialize Mastermind game file
+class Mastermind
+  COLORS = ["R", "G", "B", "Y", "O", "P"]
+
+  def initialize
+    @comp = Computer.new
+    @player = Player.new
+  end
+
+  def play
+    12.times do |i|
+      puts "This is chance #{i+1} of 12"
+
+
+class Computer
+
+  def initialize
+  end
+  
+end
+
+class Player
+
+  def guess
+    puts "Please enter your guess by choosing one of the following: "R", "G", "B", "Y", "O", "P""
+  end
+
+  def convert_guess(guess)
+    guess.split('')
+  end
+
+end
